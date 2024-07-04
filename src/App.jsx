ce86a7ca93a9e640e49a3603866abe95181ec710
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-07-04 14:04:36 +0400

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./Pages/SignIn";
+import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Profile from "./Pages/Profile";
+import Offers from "./Pages/Offers";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
